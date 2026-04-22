6a95fa69d5ea411cce6ad09941b81d18d19dbc85
nour-yaseen25 <nooryaseen510@gmail.com>
2026-04-22 18:35:38 +0300

    refactor database connection to use environment-specific configurations

--- src/database.ts
+++ src/database.ts
@@ -8,11 +8,24 @@ import dotenv from "dotenv";
 //dotenv.config() loads the environment variables from the .env file into process.env, making them accessible throughout the application.
 dotenv.config();
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD
-});
+let client:any;
+console.log(`Environment: ${process.env.ENV}`)
 
-export default pool;
+if (process.env.ENV === 'dev') {
+  client = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD
+  });
+}
+else {
+  client = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME_TEST,
+    password: process.env.DB_PASSWORD
+  });
+}
+
+export default client;
