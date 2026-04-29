e36b3ced0f3ae1e537f437fa682bce35f0617823
nour-yaseen25 <nooryaseen510@gmail.com>
2026-04-29 18:11:50 +0300

    feat: add tests for Order and Product models with database setup

--- src/models/tests/book_spec.ts
+++ src/models/tests/book_spec.ts
@@ -1,8 +1,16 @@
+import client from '../../database';
 import { Book, BookStore } from '../book';
 
 const store = new BookStore()
 
 describe("Book Model", () => {
+
+  beforeEach(async () => {
+  const conn = await client.connect();
+  await conn.query('TRUNCATE TABLE books RESTART IDENTITY CASCADE');
+  conn.release();
+});
+  
   it('should have an index method', () => {
     expect(store.index).toBeDefined();
   });
@@ -26,44 +34,62 @@ describe("Book Model", () => {
   it('create method should add a book', async () => {
     const result = await store.create({
       title: 'Bridge to Terabithia',
-      totalPages: 250,
+      total_pages: 250,
       author: 'Katherine Paterson',
       summary: 'Childrens'
     });
     expect(result).toEqual({
       id: 1,
       title: 'Bridge to Terabithia',
-      totalPages: 250,
+      total_pages: 250,
       author: 'Katherine Paterson',
       summary: 'Childrens'
     });
   });
 
   it('index method should return a list of books', async () => {
+    await store.create({
+      title: 'Bridge to Terabithia',
+      total_pages: 250,
+      author: 'Katherine Paterson',
+      summary: 'Childrens'
+    });
     const result = await store.index();
     expect(result).toEqual([{
       id: 1,
       title: 'Bridge to Terabithia',
-      totalPages: 250,
+      total_pages: 250,
       author: 'Katherine Paterson',
       summary: 'Childrens'
     }]);
   });
 
   it('show method should return the correct book', async () => {
+    await store.create({
+      title: 'Bridge to Terabithia',
+      total_pages: 250,
+      author: 'Katherine Paterson',
+      summary: 'Childrens'
+    });
     const result = await store.show(1);
     expect(result).toEqual({
       id: 1,
       title: 'Bridge to Terabithia',
-      totalPages: 250,
+      total_pages: 250,
       author: 'Katherine Paterson',
       summary: 'Childrens'
     });
   });
 
   it('delete method should remove the book', async () => {
-    store.delete(1);  
-    const result = await store.index()
+    await store.create({
+      title: 'Bridge to Terabithia',
+      total_pages: 250,
+      author: 'Katherine Paterson',
+      summary: 'Childrens'
+    });
+    await store.delete(1);
+    const result = await store.index();
 
     expect(result).toEqual([]);
   });
